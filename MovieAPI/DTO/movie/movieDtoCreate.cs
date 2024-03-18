f899e6433b9e8f98c54ae6f14cc5ab99b7891a59
Risiidhan <rkishon4@gmail.com>
2024-03-18 13:20:48 +0530

    validation for movies DTos

--- MovieAPI/DTO/movie/movieDtoCreate.cs
+++ MovieAPI/DTO/movie/movieDtoCreate.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,21 @@ namespace MovieAPI.DTO.movie
 {
     public class movieDtoCreate
     {
+        
+        private const string RequiredSentence = "is required!";
+        private const string MinLengthSentence = "must be at least 2 characters!";
+        private const string MaxLengthSentence = "must be maximum of 25 characters!";
+
+
+        [Required(ErrorMessage = $"Name {RequiredSentence}")]
+        [MinLength(2, ErrorMessage = $"Name {MinLengthSentence}")]
+        [MaxLength(25, ErrorMessage = $"Name {MaxLengthSentence}")]
         public required string Name { get; set; }
         public int? ReleasedYear { get; set; }
         public bool? IsMyFavourite { get; set; }
-         public int LeadActorID { get; set; }
+
+        [Required(ErrorMessage = $"LeadActorID {RequiredSentence}")]
+        public int LeadActorID { get; set; }
 
     }
 }
