b14f14e5cfd42c71ede0156ed29cd4c8b5448f43
Risiidhan <rkishon4@gmail.com>
2024-03-20 19:04:10 +0530

    minor fix

--- MovieAPI/obj/Debug/net8.0/MovieAPI.AssemblyInfo.cs
+++ MovieAPI/obj/Debug/net8.0/MovieAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c09efae3b5658699443bbb11c06dfac46c6072a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e8a9800ee8807e1db4768219dea14ceeef8017c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
