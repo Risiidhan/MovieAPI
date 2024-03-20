6c09efae3b5658699443bbb11c06dfac46c6072a
Risiidhan <rkishon4@gmail.com>
2024-03-20 11:52:22 +0530

    .

--- MovieAPI/obj/Debug/net8.0/MovieAPI.AssemblyInfo.cs
+++ MovieAPI/obj/Debug/net8.0/MovieAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4cd8432001667b0f959262bda76feb75f272ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+079aa63dbc2432a5d10b88811c696af86c7a7fe0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
